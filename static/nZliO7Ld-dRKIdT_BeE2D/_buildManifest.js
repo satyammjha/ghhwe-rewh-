c11fa6827c52b0fa3ec2114416bf95b667fae596
Satyam Jha <108946112+satyammjha@users.noreply.github.com>
2023-12-26 21:47:19 +0530

    Add files via upload

---
+++ static/nZliO7Ld-dRKIdT_BeE2D/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/7107d30977c7c7fd.css","static/chunks/pages/index-31b1ff260858bcd0.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
